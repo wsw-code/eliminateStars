9a0e6e10f814dab2079d80ce981d6b7221eb4ca7
wsw <602851406@qq.com>
2024-12-12 00:05:19 +0800

    feat:迭代

--- assets/scripts/Start/StartTs.ts
+++ assets/scripts/Start/StartTs.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, director, Node } from 'cc';
+import { _decorator, AssetManager, assetManager, AudioClip, Component, director, Node, Scene, SpriteFrame } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('StartTs')
@@ -11,6 +11,25 @@ export class StartTs extends Component {
     start() {
         this.btnNode.on('click', () => {
             director.loadScene('Game');
+        });
+
+        assetManager.loadBundle('resources', (err: string, budle: AssetManager.Bundle) => {
+            if (budle) {
+                console.log(budle)
+                // budle.loadScene('Game',Scene,(finished:number,total:number)=>{
+                //     console.log(finished)
+                //     console.log(total)
+                // },()=>{});
+                budle.loadDir('texture/elimination',SpriteFrame,(finished:number,total:number)=>{
+                    console.log(finished)
+                    console.log(total)
+                },()=>{});
+                budle.loadDir('texture/common')
+                budle.loadDir('texture/eliminateDie')
+                budle.loadDir('audio',AudioClip)
+            } else {
+                console.log('资源加载失败' + err);
+            }
         });
     }
 
